ae3f831d89d16515a1dcba61d911fe8b9b02175a
John Loudon <jsl245@nau.edu>
2016-03-28 09:05:08 -0700

    initial commit with 2 files

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+echo("<html><head><title>Test Page</title></head><body>");
+echo("<h1>It works!</h1>");
+echo("<p>This is a PHP driven web page for this server.</p>");
+echo("<p>The web server software is running but no content has been added, yet.</p>");
+?>
